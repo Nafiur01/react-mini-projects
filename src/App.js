5f38e760b65e40b11ccfad8a1f06051762adb9e6
Nafiur01 <nafiurrahman373@gmail.com>
2024-08-01 21:04:39 +0600

    link fix app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Accordion from './components/accordion';
+import "./App.css";
+import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
     <div className="App">
       <Accordion />
+      <RandomColor />
+      <StarRating NoOfStars={10} />
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={2} limit={10} />
+      <LoadMoreData />
+      <TreeView menus={menus} />
     </div>
   );
 }
